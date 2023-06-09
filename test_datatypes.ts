c157e5d07e0e388aef42a987a568b6a09815fffc
menquan <menquan@micron.com>
2023-06-09 14:51:03 +0800

    [chore] just trying out

--- test_datatypes.ts
+++ test_datatypes.ts
@@ -1,3 +1,12 @@
-let test_name = 'Haha';
-test_name = 5;
+"use strict";
+let test_name: string = `haha`;
+let new_name = test_name.toUpperCase();
+console.log(new_name);
+
+let age: number = 30;
+console.log(age);
+
+let numList: Array<number> = [1, 2, 3, 4, 5, 6].filter((num) => num%2==1);
+numList = numList.concat([1, 2, 3]);
+console.log(numList);
 
